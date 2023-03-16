898702de8d8d93a0b4e78c2cd56f6154b24cc718
sir-hector <krauskarol7@gmail.com>
2023-03-16 12:07:45 +0100

    add caluclate

--- app/src/main/java/com/example/tipper/MainActivity.java
+++ app/src/main/java/com/example/tipper/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable; // for EditText event handling
 import android.text.TextWatcher; // EditText listener
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText; // for bill amount input
 import android.widget.SeekBar; // for changing the tip percentage
 import android.widget.SeekBar.OnSeekBarChangeListener; // SeekBar listener
@@ -14,116 +16,37 @@ import java.text.NumberFormat; // for currency formatting
 
 public class MainActivity extends AppCompatActivity {
 
-    // currency and percent formatter objects
-    private static final NumberFormat currencyFormat =
-            NumberFormat.getCurrencyInstance();
-    private static final NumberFormat percentFormat =
-            NumberFormat.getPercentInstance();
+    private EditText editTextWeight;
+    private EditText editTextHeight;
+    private Button button;
+    private TextView textView;
 
-    private double billAmount = 0.0; // bill amount entered by the user
-    private double percent = 0.15; // initial tip percentage
-    private TextView amountTextView; // shows formatted bill amount
-    private TextView percentTextView; // shows tip percentage
-    private TextView tipTextView; // shows calculated tip amount
-    private TextView totalTextView; // shows calculated total bill amount
 
     // called when the activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // call superclass onCreate
         setContentView(R.layout.activity_main); // inflate the GUI
-//
-//        // get references to programmatically manipulated TextViews
-//        amountTextView = (TextView) findViewById(R.id.amountTextView);
-//        percentTextView = (TextView) findViewById(R.id.percentTextView);
-//        tipTextView = (TextView) findViewById(R.id.tipTextView);
-//        totalTextView = (TextView) findViewById(R.id.totalTextView);
-//        tipTextView.setText(currencyFormat.format(0));
-//        totalTextView.setText(currencyFormat.format(0));
-//
-//        // set amountEditText's TextWatcher
-//        EditText amountEditText =
-//                (EditText) findViewById(R.id.amountEditText);
-//        amountEditText.addTextChangedListener(amountEditTextWatcher);
-//
-//        // set percentSeekBar's OnSeekBarChangeListener
-//        SeekBar percentSeekBar =
-//                (SeekBar) findViewById(R.id.percentSeekBar);
-//        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
+
+        editTextWeight = findViewById(R.id.weight);
+        editTextHeight = findViewById(R.id.height);
+        button = findViewById(R.id.btnSubmit);
+        textView = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
     }
 
-    // calculate and display tip and total amounts
+    // calculate and display BMI
     private void calculate() {
-        // format percent and display in percentTextView
-        percentTextView.setText(percentFormat.format(percent));
-
-        // calculate the tip and total
-        double tip = billAmount * percent;
-        double total = billAmount + tip;
-
-        // display tip and total formatted as currency
-        tipTextView.setText(currencyFormat.format(tip));
-        totalTextView.setText(currencyFormat.format(total));
+        float height = Float.parseFloat(String.valueOf(editTextHeight.getText())) / 100;
+        float weight = Float.parseFloat(String.valueOf(editTextWeight.getText()));
+        float bmi = weight / (height * height);
+        textView.setText(String.valueOf(bmi));
     }
-
-    // listener object for the SeekBar's progress changed events
-    private final OnSeekBarChangeListener seekBarListener =
-            new OnSeekBarChangeListener() {
-                // update percent, then call calculate
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress,
-                                              boolean fromUser) {
-                    percent = progress / 100.0; // set percent based on progress
-                    calculate(); // calculate and display tip and total
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) { }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) { }
-            };
-
-    // listener object for the EditText's text-changed events
-    private final TextWatcher amountEditTextWatcher = new TextWatcher() {
-        // called when the user modifies the bill amount
-        @Override
-        public void onTextChanged(CharSequence s, int start,
-                                  int before, int count) {
-
-            try { // get bill amount and display currency formatted value
-                billAmount = Double.parseDouble(s.toString()) / 100.0;
-                amountTextView.setText(currencyFormat.format(billAmount));
-            }
-            catch (NumberFormatException e) { // if s is empty or non-numeric
-                amountTextView.setText("");
-                billAmount = 0.0;
-            }
-
-            calculate(); // update the tip and total TextViews
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) { }
-
-        @Override
-        public void beforeTextChanged(
-                CharSequence s, int start, int count, int after) { }
-    };
 }
 
-
-/*************************************************************************
- * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
