f6f6a8502b9f44e5ecec72422c66e4a4f0b88e23
sir-hector <krauskarol7@gmail.com>
2023-03-31 00:00:45 +0200

    edit recipies

--- app/src/main/java/com/example/tipper/RecipiesActivity.java
+++ app/src/main/java/com/example/tipper/RecipiesActivity.java
@@ -12,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RecipiesActivity extends AppCompatActivity {
 
     private Button mainButton;
+    private Button firstRecipe;
+    private Button secondRecipe;
     private TextView textView;
 
 
@@ -23,12 +25,26 @@ public class RecipiesActivity extends AppCompatActivity {
 
 
         mainButton = findViewById(R.id.back);
+        firstRecipe = findViewById(R.id.first_recipe);
+        secondRecipe = findViewById(R.id.second_recipe);
         textView = findViewById(R.id.result);
 
         mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchMainActivity();
+            }
+        });
+        firstRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFirstRecipe();
+            }
+        });
+        secondRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadSecondRecipe();
             }
         });
 
@@ -38,5 +54,21 @@ public class RecipiesActivity extends AppCompatActivity {
     private void launchMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+    private void loadFirstRecipe(){
+        textView.setText("Rybę pokroić na porcje, doprawić solą oraz pieprzem i obtoczyć w mące.\n" +
+                "Rozgrzać dużą patelnię, roztopić olej, zwiększyć ogień i włożyć filety.\n" +
+                "Smażyć przez ok. 2 - 3 minuty, następnie delikatnie przewrócić rybę na drugą stronę i smażyć przez ok. 2 minuty.\n" +
+                "Zmniejszyć trochę ogień pod patelnią. Pomiędzy rybę wlać na patelnię mleko kokosowe, słodki sos chili oraz sos rybny.\n" +
+                "Potrząsając delikatnie patelnią przemieszać składniki sosu. Gotować przez ok. 1 - 2 minuty. Na koniec skropić sokiem z limonki i posypać bazylią.\n" +
+                "Podawać z ugotowanym ryżem i np. z fasolką szparagową.");
+    }
+    private void loadSecondRecipe(){
+        textView.setText("Rybę pokroić na porcje, doprawić solą oraz pieprzem i obtoczyć w mące.\n" +
+                "Rozgrzać dużą patelnię, roztopić olej, zwiększyć ogień i włożyć filety.\n" +
+                "Smażyć przez ok. 2 - 3 minuty, następnie delikatnie przewrócić rybę na drugą stronę i smażyć przez ok. 2 minuty.\n" +
+                "Zmniejszyć trochę ogień pod patelnią. Pomiędzy rybę wlać na patelnię mleko kokosowe, słodki sos chili oraz sos rybny.\n" +
+                "Potrząsając delikatnie patelnią przemieszać składniki sosu. Gotować przez ok. 1 - 2 minuty. Na koniec skropić sokiem z limonki i posypać bazylią.\n" +
+                "Podawać z ugotowanym ryżem i np. z fasolką szparagową.");
     }
 }
