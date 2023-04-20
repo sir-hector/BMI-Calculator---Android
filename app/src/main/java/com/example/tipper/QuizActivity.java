542e556fb5fd8c2bc582a8c03d85414f344db8bc
sir-hector <krauskarol7@gmail.com>
2023-04-20 23:26:40 +0200

    add questions and answers

--- app/src/main/java/com/example/tipper/QuizActivity.java
+++ app/src/main/java/com/example/tipper/QuizActivity.java
@@ -1,16 +1,134 @@
 package com.example.tipper;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class QuizActivity extends AppCompatActivity {
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+
+    public static String question[] ={
+            "What are some common sources of unhealthy fats?",
+            "Which of the following is a recommended daily intake of fruits and vegetables?",
+            "How can someone make healthy choices while grocery shopping?",
+            "Which of the following is a good source of carbohydrates?",
+            "How much water should an individual drink per day?",
+            "Which of the following is a healthy protein source for individuals who do not eat meat?",
+    };
+
+    public static String choices[][] = {
+            {"Saturated and Trans fats","Unsaturated fats","Omega-3 fatty acids"," None of the above"},
+            {"1-2 servings","3-4 servings"," 5-6 servings","7-8 servings"},
+            {"Shopping the perimeter of the store","Reading food labels"," Buying in-season produce","All of the above"},
+            {"White bread","Pasta","Brown rice","Potato chips"},
+            {"4-6 cups","6-8 cups","8-10 cups","10-12 cups"},
+            {"Tofu","Chicken","Salmon","Beef"}
+    };
+
+    public static String correctAnswers[] = {
+            "Saturated and Trans fats",
+            "5-6 servings",
+            "All of the above",
+            "Brown rice",
+            "8-10 cups",
+            "Tofu"
+    };
+
+    int score=0;
+    int totalQuestion = question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // call superclass onCreate
-        setContentView(R.layout.quiz_layout); // inflate the GUI
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_layout);
 
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions : " +totalQuestion);
+        loadNewQuestion();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit_btn){
+            if(selectedAnswer.equals(correctAnswers[currentQuestionIndex])){
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+        }else{
+            selectedAnswer  = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+
+        }
+
+    }
+
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+        questionTextView.setText(question[currentQuestionIndex]);
+        ansA.setText(choices[currentQuestionIndex][0]);
+        ansB.setText(choices[currentQuestionIndex][1]);
+        ansC.setText(choices[currentQuestionIndex][2]);
+        ansD.setText(choices[currentQuestionIndex][3]);
+
+    }
+
+    void finishQuiz(){
+        String passStatus = "";
+        if(score > totalQuestion*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is "+ score+" out of "+ totalQuestion)
+                .setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+    }
+
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex =0;
+        loadNewQuestion();
     }
 }
